13b326337ac1d07a00a003f88c36aa372f6ded87
nan0s7 <scottwalters97@gmail.com>
2017-10-30 21:36:52 +1100

    More efficient

--- lb.sh
+++ lb.sh
@@ -2,9 +2,11 @@
 
 slp_track="0"
 slp="2"
-max_st="60"
+max_st="$[ 59 / $slp ]"
+max_sleep="$max_st"
 tmp=""
-old_tmp=""
+old_per=""
+old_desk="0"
 update_now="0"
 
 declare -a cmd_arr=()
@@ -12,7 +14,9 @@ declare -a cmd_arr=()
 function finish {
 	unset per
 	unset tmp
-	unset old_tmp
+	unset old_per
+	unset old_desk
+	unset update_now
 	unset cur
 	unset wind
 	unset desk
@@ -24,81 +28,116 @@ function finish {
 trap finish EXIT
 
 function get_focused_window {
-	tmp="$wind"
-	wind=$(xdotool getwindowfocus getwindowname)
-	if [ "$tmp" != "$wind" ] && [ "$update_now" -ne "1" ]; then
+	tmp=$(xdotool getwindowfocus getwindowname)
+	if [ "$tmp" != "$wind" ]; then #&& [ "$update_now" -ne "1" ]; then
+		wind="$tmp"
 		update_now="1"
 	fi
 	unset tmp
 }
 
 function get_battery {
-#    tmp="$per"
-	per=$(acpi --battery) #| cut -d, -f2)
-    old_tmp="$per"
-    if [ "$old_tmp" != "$per" ] && [ "$update_now" -ne "1" ]; then
+	tmp=$(acpi --battery) #| cut -d, -f2)
+    if [ "$old_per" != "$tmp" ]; then #&& [ "$update_now" -ne "1" ]; then
+	    old_per="$per"
+	    per="$tmp"
 	    per=${per:11}
 	    update_now="1"
 	fi
+	unset tmp
 }
 
 function get_date {
-	tmp="$cur"
-	cur=$(date "+%a %b %d, %H:%M")
-	if [ "$tmp" != "$cur" ] && [ "$update_now" -ne "1" ]; then
+	tmp=$(date "+%a %b %d, %H:%M")
+	if [ "$tmp" != "$cur" ]; then #&& [ "$update_now" -ne "1" ]; then
+		cur="$tmp"
 		update_now="1"
 	fi
 	unset tmp
 }
 
 function get_desktop {
-#	tmp="$desk"
-	desk=$(wmctrl -d | grep "*")
-    old_tmp="$desk"
-	if [ "$old_tmp" != "$desk" ] && [ "$update_now" -ne "1" ]; then
+	tmp=$(wmctrl -d | grep "*")
+	if [ "$old_desk" != "$tmp" ]; then #&& [ "$update_now" -ne "1" ]; then
+	    old_desk="$desk"
+	    desk="$tmp"
 	    desk=${desk:0:1}
 		update_now="1"
 	fi
 	unset tmp
 }
 
+function get_offset {
+	offset=$(date "+%S")
+	offset=$[ 59 - $offset ]
+	offset=$[ $offset / $slp ]
+}
+
+function maintain_time_sync {
+	tmp=$(date "+%S")
+	if [ "$tmp" -gt "2" ]; then
+		max_st="$[ $max_st - 1 ]"
+	elif [ "$tmp" -ge "0" ] || [ "$tmp" -le "2" ]; then
+		max_st="$max_sleep"
+	fi
+}
+
+function init_values {
+	get_offset
+	get_date
+	get_battery
+	get_desktop
+	get_focused_window
+}
+
 function update_bar {
-	echo "%{l} [$desk] $wind""%{c}$cur""%{r}$per "
+	if [ "$update_now" -eq "1" ]; then
+		echo "%{l} [$desk] $wind""%{c}$cur""%{r}$per "
+		update_now="0"
+	fi
 }
 
 function set_arr {
+	tmp="get_battery;"
 	for i in `seq 0 $max_st`; do
-		if [ "$[ $i % 20 ]" -eq "0" ]; then
-			cmd_arr+=( "get_battery;" )
-		fi
-		cmd_arr+=( "get_desktop; get_date;" )
-		cmd_arr+=( "get_date;" )
-		cmd_arr+=( "get_focused_window; get_desktop;" )
+		tmp="$tmp""get_date;get_desktop;get_focused_window;"
+		cmd_arr+=( "$tmp" )
+		tmp=""
 	done
 }
 
 function main {
 #	set_arr
 
-	get_battery
-	cur=0
+#	get_battery
+#	cur=0
+	init_values
+	update_bar
 
-	while true; do
-		eval ${cmd_arr[$slp_track]}
-#		cur=$[ $cur + 1 ]
-		get_date
+	for i in `seq 0 $offset`; do
 		get_focused_window
 		get_desktop
-		slp_track=$[ $slp_track + $slp ]
-		if [ "$slp_track" -eq "$max_st" ]; then
-			get_battery
-			slp_track=0
-		fi
-		if [ "$update_now" -eq "1" ]; then
-			update_now="0"
-			update_bar
-		fi
+		update_bar
 		sleep "$slp"
+	done
+
+	while true; do
+		get_date
+		get_battery
+		maintain_time_sync
+		for i in `seq 0 $max_st`; do
+#			eval ${cmd_arr[$i]}
+#			cur=$[ $cur + 1 ]
+			get_focused_window
+			get_desktop
+#			if [ "$slp_track" -eq "$max_st" ]; then
+#				get_battery
+#				slp_track=0
+#			fi
+			update_bar
+#			slp_track=$[ $slp_track + $slp ]
+			sleep "$slp"
+		done
 	done
 }
 
