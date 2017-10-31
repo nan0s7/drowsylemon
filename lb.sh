21853e930d53782abe45f561685d3fecb75cbd48
nan0s7 <scottwalters97@gmail.com>
2017-10-31 22:39:01 +1100

    Big update. Increased sleep time to 5 seconds

--- lb.sh
+++ lb.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 # Modify these if you like
-slp="2"
+slp="5"
 time_scale="59"
 
 # Don't change these
 tmp=""
+min="0"
 offset="0"
-old_per="0"
-old_desk="0"
+new_per="0"
+new_desk="0"
 update_now="0"
 
 options="$1"
-max_sleep="$minute_scaled"
 minute_scaled="$[ $time_scale / $slp ]"
-
 declare -a cmd_arr=()
 
 function finish {
 	unset per
 	unset tmp
 	unset time_scale
-	unset max_sleep
-	unset old_per
-	unset old_desk
+	unset new_per
+	unset new_desk
 	unset update_now
 	unset cur
 	unset offset
@@ -35,7 +33,7 @@ function finish {
 trap finish EXIT
 
 function get_focused_window {
-	tmp=$(xdotool getwindowfocus getwindowname)
+	tmp="$(xdotool getwindowfocus getwindowname)"
 	if [ "$tmp" != "$wind" ]; then
 		wind="$tmp"
 		update_now="1"
@@ -44,19 +42,15 @@ function get_focused_window {
 }
 
 function get_battery {
-	tmp=$(acpi --battery)
-    if [ "$old_per" != "$tmp" ]; then
-	    old_per="$per"
-	    per="$tmp"
-	    #per=${per:11}
-	    per=`echo $tmp | cut -d, -f2`
+    new_per="$(acpi --battery | cut -d, -f2)"
+    if [ "$new_per" != "$per" ]; then
+	    per="$new_per"
 	    update_now="1"
 	fi
-	unset tmp
 }
 
 function get_date {
-	tmp=$(date "+%a %b %d, %H:%M")
+	tmp="$(date '+%a %b %d, %H:%M')"
 	if [ "$tmp" != "$cur" ]; then
 		cur="$tmp"
 		update_now="1"
@@ -65,29 +59,31 @@ function get_date {
 }
 
 function get_desktop {
-	tmp=$(wmctrl -d | grep "*")
-	if [ "$old_desk" != "$tmp" ]; then
-	    old_desk="$desk"
-	    desk="$tmp"
-	    desk=${desk:0:1}
+	new_desk="$(wmctrl -d | grep '*')"
+	new_desk="${new_desk:0:1}"
+	if [ "$new_desk" != "$desk" ]; then
+	    desk="$new_desk"
 		update_now="1"
 	fi
-	unset tmp
 }
 
 function get_offset {
-	offset=$(date "+%S")
+	offset="$(date '+%S')"
 	offset="$[ $time_scale - $offset ]"
 	offset="$[ $offset / $slp ]"
 }
 
-function maintain_time_sync {
-	tmp=$(date "+%S")
-	if [ "$tmp" -gt "2" ]; then
+function sync_time_update {
+	tmp="$(date '+%S')"
+	if [ "$tmp" -gt "5" ]; then
+		minute_scaled="$[ $time_scale - $tmp + 2 ]"
+		minute_scaled="$[ $minute_scaled / $slp ]"
+	elif [ "$tmp" -gt "2" ]; then
 		minute_scaled="$[ $minute_scaled - 1 ]"
-	elif [ "$tmp" -ge "0" ] || [ "$tmp" -le "2" ]; then
-		minute_scaled="$max_sleep"
+	else
+		minute_scaled="$[ $time_scale / $slp ]"
 	fi
+#	elif [ "$tmp" -ge "0" ] || [ "$tmp" -le "2" ]; then
 	unset tmp
 }
 
@@ -96,12 +92,12 @@ function init_values {
 	get_date
 	get_battery
 	get_desktop
-	get_focused_window
+#	get_focused_window
 }
 
 function update_bar {
 	if [ "$update_now" -eq "1" ]; then
-		echo "%{l} [$desk] $wind""%{c}$cur""%{r}$per "
+		echo "%{l} [$desk]""%{c}$cur""%{r}$per "
 		update_now="0"
 	fi
 }
@@ -110,22 +106,29 @@ function main {
 	init_values
 
 	for i in `seq 0 $offset`; do
-		get_focused_window
+#		get_focused_window
 		get_desktop
 		update_bar
 		sleep "$slp"
 	done
+	unset offset
+
+	sync_time_update
 
 	while true; do
 		get_date
-		get_battery
-		maintain_time_sync
+		if [ "$min" -eq "5" ]; then
+			get_battery
+			sync_time_update
+			min="0"
+		fi
 		for i in `seq 0 $minute_scaled`; do
-			get_focused_window
+#			get_focused_window
 			get_desktop
 			update_bar
 			sleep "$slp"
 		done
+		min="$[ $min + 1 ]"
 	done
 }
 
