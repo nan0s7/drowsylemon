9485be601c0825d81adc0029fc876661d49876cf
nan0s7 <scottwalters97@gmail.com>
2017-11-07 20:37:28 +1100

    Now using slightly more efficient commands

--- lb.sh
+++ lb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modify these if you like
-slp="5"
+slp="1"
 time_scale="59"
 
 # Don't change these
@@ -16,7 +16,7 @@ options="$1"
 minute_scaled="$[ $time_scale / $slp ]"
 declare -a cmd_arr=()
 
-function finish {
+finish() {
 	unset per
 	unset tmp
 	unset time_scale
@@ -34,49 +34,58 @@ function finish {
 }
 trap finish EXIT
 
-function get_focused_window {
-	tmp="$(xdotool getwindowfocus getwindowname)"
-	if [ "$tmp" != "$wind" ]; then
-		wind="$tmp"
-		update_now="1"
-	fi
-	unset tmp
+get_focused_window() {
+	tmp="$(xprop -root _NET_ACTIVE_WINDOW)"
+	tmp="$(xprop -id ${tmp#*# *} _NET_WM_NAME)"
+	tmp="${tmp#*= *}"
+	tmp="${tmp//\"}"
+	#tmp="$(xdotool getwindowfocus getwindowname)"
+#	if [ "$tmp" != "$wind" ]; then
+	wind="$tmp"
+#		update_now="1"
+#	fi
+#	unset tmp
 }
 
-function get_battery {
-    new_per="$(acpi --battery | cut -d, -f2)"
-    if [ "$new_per" != "$per" ]; then
-	    per="$new_per"
-	    update_now="1"
-	fi
+get_battery() {
+    tmp="$(acpi --battery | cut -d, -f2)"
+#    if [ "$tmp" != "$per" ]; then
+	per="$tmp"
+#	    update_now="1"
+#	fi
 }
 
-function get_date {
+get_date() {
 	tmp="$(date '+%a %b %d, %H:%M')"
-	if [ "$tmp" != "$cur" ]; then
-		cur="$tmp"
-		update_now="1"
-	fi
-	unset tmp
+#	if [ "$tmp" != "$cur" ]; then
+	cur="$tmp"
+#		update_now="1"
+#	fi
+#	unset tmp
 }
 
-function get_desktop {
-	new_desk="$(wmctrl -d | grep '*')"
-	new_desk="${new_desk:0:1}"
+get_desktop() {
+	tmp="$(wmctrl -d)"
+	tmp="${tmp%*  \* *}"
+#	tmp="${tmp: -1}"
+	tmp="$[ ${tmp: -1} + 1 ]"
+#	new_desk="$(wmctrl -d | grep '*')"
+#	new_desk="${new_desk:0:1}"
+#	new_desk="$[ $new_desk + 1 ]"
 #	new_desk="$(xdotool get_desktop)"
-	if [ "$new_desk" != "$desk" ]; then
-	    desk="$new_desk"
-		update_now="1"
-	fi
+#	if [ "$tmp" != "$desk" ]; then
+	desk="$tmp"
+#		update_now="1"
+#	fi
 }
 
-function get_offset {
+get_seconds_offset() {
 	offset="$(date '+%S')"
 	offset="$[ $time_scale - $offset ]"
 	offset="$[ $offset / $slp ]"
 }
 
-function sync_time_update {
+sync_time_update() {
 	tmp="$(date '+%S')"
 	if [ "$tmp" -gt "5" ]; then
 		minute_scaled="$[ $time_scale - $tmp + 2 ]"
@@ -87,29 +96,29 @@ function sync_time_update {
 		minute_scaled="$[ $time_scale / $slp ]"
 	fi
 #	elif [ "$tmp" -ge "0" ] || [ "$tmp" -le "2" ]; then
-	unset tmp
+#	unset tmp
 }
 
-function init_values {
-	get_offset
+init_values() {
+	get_seconds_offset
 	get_date
 	get_battery
 	get_desktop
-#	get_focused_window
+	get_focused_window
 }
 
-function update_bar {
-	if [ "$update_now" -eq "1" ]; then
-		echo "%{l} [$desk]""%{c}$cur""%{r}$per "
-		update_now="0"
-	fi
+update_bar() {
+#	if [ "$update_now" -eq "1" ]; then
+	echo "%{l} [$desk] $wind""%{c}$cur""%{r}$per "
+#		update_now="0"
+#	fi
 }
 
-function main {
+main() {
 	init_values
 
 	for i in `seq 0 $offset`; do
-#		get_focused_window
+		get_focused_window
 		get_desktop
 		update_bar
 		sleep "$slp"
@@ -126,7 +135,7 @@ function main {
 			min="0"
 		fi
 		for i in `seq 0 $minute_scaled`; do
-#			get_focused_window
+			get_focused_window
 			get_desktop
 			update_bar
 			sleep "$slp"
