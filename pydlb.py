46116449f1a8b0d25e3ca05f9c54db2b4e741fec
nan0s7 <scottwalters97@gmail.com>
2017-11-15 14:39:33 +1100

    Updated logic & added desktop information

--- pydlb.py
+++ pydlb.py
@@ -2,7 +2,6 @@ import subprocess, time
 
 def print_bar_information(info):
 	subprocess.call(["echo", info])
-#	print(info)
 
 def set_left_information(info):
 	left = "%{l}" + str(info)
@@ -18,19 +17,19 @@ def set_right_information(info):
 
 def get_time_offset(time_scale, sleep):
 	offset = subprocess.run(["date", "+%S"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-	offset = round((time_scale - int(offset)) / sleep)
-#	print("offset: " + str(offset))
+	offset = -(-(time_scale - int(offset)) // sleep)
+	offset = offset + 3
 	return offset
 
 def align_time(time_scale, sleep, minute_scaled):
 	tmp = subprocess.run(["date", "+%S"], stdout=subprocess.PIPE).stdout.decode('utf-8')
 	tmp = int(tmp)
 	if tmp > 5:
-		return (time_scale - tmp + 2) / sleep
+		return round((time_scale - tmp + 2) / sleep)
 	elif tmp > 2:
 		return minute_scaled - 1
 	else:
-		return time_scale / sleep
+		return round(time_scale / sleep)
 
 def get_date():
 	tmp = subprocess.run(["date", "+%a %b %d, %H:%M"], stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -42,36 +41,116 @@ def get_battery():
 	comma = tmp.find(",")
 	return tmp[comma + 2: per + 1]
 
+def get_desktop():
+	tmp = subprocess.run(["wmctrl", "-d"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+	return tmp[tmp.find("*") - 3]
+
+def set_bar_information(left, middle, right):
+	info = \
+		set_left_information(left) +\
+		set_middle_information(middle) +\
+		set_right_information(right)
+	return info
+
+def check_updatable(new_info, old_info):
+	tmp = False
+#	for i in range(len(new_info)):
+	if new_info != old_info:
+		tmp = True
+	if tmp == True:
+#		set_bar_information(left_info, middle_info, right_info)
+#		bar_information = \
+#			set_left_information(new_info[2]) +\
+#			set_middle_information(new_info[0]) +\
+#			set_right_information(new_info[1])
+		print_bar_information(new_info)
+	return tmp
 
 def main():
 	sleep = 1
 	time_scale = 59
 	minute_scaled = 0
 	bar_information = ""
+	old_information = ""
+	bar_battery = ""
+	bar_date = ""
+	bar_desktop = ""
+#	information_array = []
+	left_info = ""
+	middle_info = ""
+	right_info = ""
 
 	offset = get_time_offset(time_scale, sleep)
-	minute_scaled = align_time(time_scale, sleep, minute_scaled)
+#	minute_scaled = align_time(time_scale, sleep, minute_scaled)
 
+	# init values
+#	information_array.append(get_date())
+#	information_array.append(get_battery())
+#	information_array.append(get_desktop())
+	left_info = get_desktop()
+	middle_info = get_date()
+	right_info = get_battery()
+	bar_information = set_bar_information(left_info, middle_info, right_info)
+#	bar_information = \
+#		set_left_information(left_info) +\
+#		set_middle_information(middle_info) +\
+#		set_right_information(right_info)
+	# expand old_information
+#	for i in range(len(information_array)):
+#		old_information.append("")
+
+	# fix time
 	for i in range(offset):
-		bar_information = \
-			set_middle_information(get_date()) +\
-			set_right_information(get_battery())
-		print_bar_information(bar_information)
-		time.sleep(1)
+#		bar_desktop = get_desktop()
+#		information_array[2] = bar_desktop
+		left_info = get_desktop()
+#		print(bar_information)
+		bar_information = set_bar_information(left_info, middle_info, right_info)
+#		print(bar_information)
+		tmp = check_updatable(bar_information, old_information)
+		if tmp == True:
+			old_information = bar_information
+#			print("updated in offset")
+		time.sleep(sleep)
 
-	align_time(time_scale, sleep, minute_scaled)
+#	minute_scaled = align_time(time_scale, sleep, minute_scaled)
 
-#	ctime = get_date()
+	# force an update
+#	information_array = []
+#	information_array.append(get_date())
+#	information_array.append(get_battery())
+#	information_array.append(get_desktop())
 
-#	print("battery: " + str(get_battery()))
-#	print("date: " + str(get_date()))
+#	left_info = get_desktop()
+#	middle_info = get_date()
+#	right_info = get_battery()
+#	set_bar_information(left_info, middle_info, right_info)
+#	tmp = check_updatable(bar_information, old_information)
+#	if tmp:
+#		old_information = bar_information
 
 	while True:
-		align_time(time_scale, sleep, minute_scaled)
-		bar_information = \
-			set_middle_information(get_date()) +\
-			set_right_information(get_battery())
-		print_bar_information(bar_information)
-		time.sleep(1)
+		minute_scaled = align_time(time_scale, sleep, minute_scaled)
+
+		middle_info = get_date()
+		right_info = get_battery()
+#		information_array = []
+#		information_array.append(get_date())
+#		information_array.append(get_battery())
+		for i in range(minute_scaled):
+#			bar_desktop = get_desktop()
+#			if len(information_array) == 2:
+#				information_array.append(bar_desktop)
+#			else:
+#				information_array[2] = bar_desktop
+			left_info = get_desktop()
+			bar_information = set_bar_information(left_info, middle_info, right_info)
+			tmp = check_updatable(bar_information, old_information)
+			if tmp == True:
+				old_information = bar_information
+#				print("updated in main")
+			time.sleep(sleep)
 
 main()
+
+# need to seperate updates of individual information
