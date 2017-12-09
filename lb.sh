7b5e22dcb3d41828cfd424b0c9e5803e91ef7f15
nan0s7 <scottwalters97@gmail.com>
2017-12-09 18:29:02 +1100

    Added option to display windows for the current desktop only.. WIP

--- lb.sh
+++ lb.sh
@@ -6,7 +6,7 @@ time_scale="59"
 
 # Don't change these
 #tmp=""
-min="0"
+#min="0"
 offset="0"
 #new_per="0"
 #new_desk="0"
@@ -24,19 +24,23 @@ minute_scaled="$[ $time_scale / $slp ]"
 # make configuration more modular
 # keep functionality while removing the information array
 # (reduce computation time by a decent amount, that way)
+# add in more pretty things for the default config
+# redo the get_desktop function
+# possibly make functions even more general so that they can be used in
+# more flexible circumstances
 
 finish() {
 	unset per
 	unset tmp
 	unset time_scale
 #	unset new_per
-	unset min
+#	unset min
 #	unset options
 #	unset new_desk
 	unset update_now
-	unset cur
+#	unset cur
 	unset offset
-	unset wind
+#	unset wind
 	unset desk
 	unset slp
 	unset minute_scaled
@@ -68,21 +72,20 @@ trap finish EXIT
 
 get_battery() {
 	# This command will change in the future
-    per="$(acpi --battery | cut -d, -f2)"
-    echo "$per"
+    echo "$(acpi --battery | cut -d, -f2)"
 }
 
 get_date() {
-	cur="$(date '+%a %b %d, %H:%M')"
-	echo "$cur"
+	echo "$(date '+%a %b %d, %H:%M')"
 }
 
-#get_desktop() {
-#	tmp="$(wmctrl -d)"
-#	tmp="${tmp%*  \* *}"
-#	desk="${tmp: -1}"
+# make this better so I don't have to use xdotool
+get_desktop() {
+	tmp="$(wmctrl -d)"
+	tmp="${tmp%*  \* *}"
+	desk="${tmp: -1}"
 #	update_now="1"
-#}
+}
 
 declare -a task_hexs=()
 declare -a task_wins=()
@@ -91,49 +94,31 @@ pc_name="$(uname -n)"
 max_task_len="100"
 active_win=""
 old_active_win=""
-#win_num=""
-#old_win_num=""
 win_list=""
 active_col="%{B#545454}"
 not_col="%{B-}"
 format_len="4"
 cdo="1"
 
+# maybe reduce array size if cdo=1 for less computation too later
 get_tasks() {
-	# I would like to change the win_num calculation someday
-	# could remove it and just add array comparison for format_tasks
-#	win_num="$(ps -u --no-headers | wc -l)"
 	get_active_win
-	if [ "$active_win" != "$old_active_win" ]; then #|| [ "$old_win_num" != "$win_num" ]; then
-		IFS='
-		'
+	if [ "$active_win" != "$old_active_win" ]; then
 		win_list="$(wmctrl -l)"
-		win_list="${win_list//$pc_name/}"
-#		old_win_num="$win_num"
 		old_active_win="$active_win"
 		task_hexs=()
 		task_wins=()
-		if [ "$cdo" -eq "1" ]; then
-			for line in $win_list; do
-				tmp="${line:0:10}"
-				if [ "$(wattr m $tmp; echo $?)" -eq "0" ]; then
-	#			win_desk="${line%% *}"
-	#			if [ "$win_desk" == "$desk" ]; then
-					task_hexs+=( "$tmp" )
-					line="${line:12}"
-					win_name="${line#${line%% *}*}"
-					task_wins+=( "${win_name:2}" )
-				fi
-			done
-		else
-			for line in $win_list; do
-				task_hexs+=( "${line:0:10}" )
-				line="${line:12}"
-				win_desk="${line%% *}"
-				win_name="${line#$win_desk*}"
-				task_wins+=( "$win_desk""${win_name:2}" )
-			done
-		fi
+#				if [ "$(wattr m $tmp; echo $?)" -eq "0" ]; then
+		win_list="${win_list//$pc_name/}"
+		IFS=$'\n'
+#		if [ "$cdo" -eq "1" ]; then
+		for line in $win_list; do
+			task_hexs+=( "${line:0:10}" )
+			line="${line:12}"
+			win_desk="${line%% *}"
+			win_name="${line#$win_desk*}"
+			task_wins+=( "$win_desk""${win_name:2}" )
+		done
 		format_tasks
 	fi
 }
@@ -147,11 +132,13 @@ format_tasks() {
 	# still needs work but it'll do for now
 	tmp="${#task_hexs[@]}"
 	if [ "$tmp" -gt "0" ]; then
-		win_len="$[ $max_task_len / ${#task_hexs[@]} ]"
+		win_len="$[ $max_task_len / $tmp ]"
 	else
 		win_len="0"
 	fi
-	for i in `seq 0 $[ ${#task_hexs[@]} - 1 ]`; do
+	# make this better
+	desk="$(xdotool get_desktop)" # put in an if
+	for i in `seq 0 $[ $tmp - 1 ]`; do
 		hex_i="${task_hexs[$i]}"
 		win_i="${task_wins[$i]}"
 		spacer=""
@@ -167,30 +154,30 @@ format_tasks() {
 		if [ "$active_win" = "$hex_i" ]; then
 			if [ "$cdo" -eq "0" ]; then
 				# can probably get rid of desk variable at some point
-				desk="${win_i:0:1}"
-				tasks+=" $desk) $active_col"
+				# or add it back in or something
+				tasks+=" ${win_i:0:1}"
+				tasks+=") $active_col"
 				tasks+="${win_i:1:$cut_len}$spacer$not_col"
 			else
-				cut_len="$[ $cut_len + 1 ]"
 				tasks+=" $active_col"
-				tasks+="${win_i:0:$cut_len}$spacer$not_col"
+				tasks+="${win_i:1:$cut_len}$spacer$not_col"
 			fi
 		else
+			win_desk="${win_i:0:1}"
 			if [ "$cdo" -eq "0" ]; then
-				tasks+=" ${win_i:0:1}) "
+				tasks+=" $win_desk) "
 				tasks+="%{A:wmctrl -a $hex_i -i; xdotool windowactivate $hex_i:}"
 				tasks+="${win_i:1:$cut_len}$spacer%{A}"
 			else
-				cut_len="$[ $cut_len + 1 ]"
-				tasks+=" "
-				tasks+="%{A:wmctrl -a $hex_i -i; xdotool windowactivate $hex_i:}"
-				tasks+="${win_i:0:$cut_len}$spacer%{A}"
+				# obvs cdo=1 will still have click events later
+				if [ "$win_desk" -eq "$desk" ]; then
+					tasks+=" ${win_i:1:$cut_len}$spacer"
+				fi
 			fi
 			# command on left click
 		fi
 	done
 	echo "$tasks"
-#	update_now="1"
 }
 
 get_seconds_offset() {
@@ -242,12 +229,6 @@ try_update() {
 	fi
 }
 
-#init_values() {
-#	get_seconds_offset
-#	get_date
-#	get_battery
-#	get_tasks
-#}
 init_values() {
 	# second offsets isn't to be displayed on panel
 	get_seconds_offset
@@ -258,8 +239,7 @@ init_values() {
 	information+=( "$(get_battery)" )
 }
 
-run_commands() {
-#	get_tasks
+run_min_cmds() {
 	try_update "$(get_tasks)" "${information[0]}" "0"
 	# get_tasks already checks for change so don't use try_update
 #	update_information "0" "$(get_tasks)"
@@ -271,26 +251,22 @@ main() {
 	init_values
 
 	for i in `seq 0 $offset`; do
-		run_commands
+		run_min_cmds
 	done
 	unset offset
 
 	sync_time_update
 
 	while true; do
-#		get_date
-		update_information "1" "$(get_date)"
-		if [ "$min" -eq "5" ]; then
-#			get_battery
-			update_information "2" "$(get_battery)"
-			sync_time_update
-			min="0"
-		fi
+		for i in `seq 0 4`; do
+			update_information "1" "$(get_date)"
+			for i in `seq 0 $minute_scaled`; do
+				run_min_cmds
+			done
+		update_information "2" "$(get_battery)"
+		sync_time_update
 		update_now="1"
-		for i in `seq 0 $minute_scaled`; do
-			run_commands
 		done
-		min="$[ $min + 1 ]"
 	done
 }
 
