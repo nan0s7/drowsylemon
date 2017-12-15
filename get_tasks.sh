be74456d2e5a1ef681a2171217986f5728cd8da7
nan0s7 <scottwalters97@gmail.com>
2017-12-15 21:41:22 +1100

    First commit of external plugin, also made more efficient

---
+++ get_tasks.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+active_win="$(pfw)"
+if [ "$active_win" != "$old_active_win" ]; then
+	declare -a task_hexs=()
+	declare -a task_wins=()
+	declare -a task_desk=()
+	declare -a consts=( "100" "%{B#545454}" "0" )
+	win_list="$(wmctrl -l)"
+	win_list="${win_list//$(uname -n)/}"
+	old_active_win="$active_win"
+	win_len="0"
+	tasks=""
+	IFS=$'\n'
+	for line in $win_list; do
+		tmp="${line:0:10}"
+		line="${line:12}"
+		win_desk="${line%% *}"
+		task_hexs+=( "$tmp" )
+		win_name="${line#$win_desk*}"
+		if [ "$tmp" = "$active_win" ]; then
+			cdesk="$win_desk"
+		fi
+		task_desk+=( "$win_desk" )
+		task_wins+=( "${win_name:2}" )
+	done
+	win_count="${#task_hexs[@]}"
+	for i in `seq 0 $win_count`; do
+		if [ "$cdesk" = "${task_desk[$i]}" ]; then
+			win_len="$[ $win_len + 1 ]"
+		fi
+	done
+	if [ "$win_len" -gt "0" ]; then
+		win_len="$[ ${consts[0]} / $win_len ]"
+	else
+		win_len="0"
+	fi
+	for i in `seq 0 $win_count`; do
+		hex_i="${task_hexs[$i]}"
+		win_i="${task_wins[$i]}"
+		if [ "${task_desk[$i]}" = "$cdesk" ]; then
+			name_len="$[ ${#win_i} + ${consts[2]} ]"
+			if [ "$name_len" -lt "$win_len" ]; then
+				spacer=""
+				spacer_len="$[ $win_len - $name_len - ${consts[2]} ]"
+				for i in `seq 0 $spacer_len`; do
+					spacer+=" "
+				done
+				if [ "$active_win" = "$hex_i" ]; then
+					tasks+=" ${consts[1]}"
+					tasks+="$win_i$spacer%{B-}"
+				else
+					tasks+=" $win_i$spacer"
+				fi
+			else
+				if [ "$active_win" = "$hex_i" ]; then
+					tasks+=" ${consts[1]}"
+					tasks+="${win_i:0:$win_len}$spacer%{B-}"
+				else
+					tasks+=" ${win_i:0:$win_len}"
+				fi
+			fi
+		fi
+	done
+	echo "$tasks"
+fi
+
