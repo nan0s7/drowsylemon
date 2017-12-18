ef91898d08cc750574bda6bdbf51256e413f893b
nan0s7 <scottwalters97@gmail.com>
2017-12-18 12:43:56 +1100

    Cleaned up some bits and added some comments

--- lb.sh
+++ lb.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
+# How long to sleep for
 slp="1"
+# How many seconds in a minute minus one
 time_scale="59"
+
+# Variable initialisation
 offset="0"
 update_now="1"
-old_bar_text=""
-bar_text=""
-
+declare -a information=()
 minute_scaled="$[ $time_scale / $slp ]"
-
-# long-term goals:
-# reduce number of tasks per second
-# make configuration more modular
-# keep functionality while removing the information array
-# (reduce computation time by a decent amount, that way)
-# add in more pretty things for the default config
-# redo the get_desktop function
-# possibly make functions even more general so that they can be used in
-# more flexible circumstances
 
 finish() {
 	unset per
@@ -40,7 +32,6 @@ get_date() {
 	echo "$(date '+%a %b %d, %H:%M')"
 }
 
-# make this better so I don't have to use xdotool
 #get_desktop() {
 #	desk="$(wmctrl -d)"
 #	desk="${desk%*  \* *}"
@@ -104,7 +95,8 @@ init_values() {
 	information+=( "$(get_battery)" )
 }
 
-run_min_cmds() {
+run_sec_cmds() {
+	# Checks if the information is stale before updating
 	try_update "$(./get_tasks.sh)" "{information[0]}" "0"
 	update_bar
 	sleep "$slp"
@@ -113,19 +105,24 @@ run_min_cmds() {
 main() {
 	init_values
 
+	# Align when script checks for the current time to be accurate
 	for i in `seq 0 $offset`; do
-		run_min_cmds
+		# Means run commands that should execute/update every second
+		run_sec_cmds
 	done
 	unset offset
 
+	# Adjusts for computation time
 	sync_time_update
 
 	while true; do
 		for i in `seq 0 4`; do
+			# Doesn't bother checking if the command gave a new value
 			update_information "1" "$(get_date)"
+			# Essentially forces an update
 			update_now="1"
-			for i in `seq 0 $minute_scaled`; do
-				run_min_cmds
+			for j in `seq 0 $minute_scaled`; do
+				run_sec_cmds
 			done
 		done
 		update_information "2" "$(get_battery)"
