8943c0e096ed3ed76a5e98d3a8ffd3480a576c5e
nan0s7 <scottwalters97@gmail.com>
2017-12-27 23:09:16 +1100

    Added new desktop functionality

---
+++ get_desktop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+update_desktop() {
+	result=""
+	# expand used_desks
+	for i in `seq 0 $desk_range`; do
+		if [ "${used_desks:$i:1}" -eq "1" ]; then
+			icon="$icon_used"
+		else
+			icon="$icon_empty"
+		fi
+		if [ "$i" -eq "$cdesk" ]; then
+			desktop_string+="%{R} $icon %{R}"
+		else
+			desktop_string+=" $icon "
+		fi
+	done
+	echo -e "$desktop_string"
+}
