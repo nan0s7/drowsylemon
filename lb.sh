0bf881862e9673b0b72f97567f20b43f0eb0f25b
nan0s7 <scottwalters97@gmail.com>
2017-12-27 23:08:21 +1100

    Changed plugin method for the better

--- lb.sh
+++ lb.sh
@@ -8,7 +8,7 @@ time_scale="59"
 # Variable initialisation
 offset="0"
 update_now="1"
-declare -a information=()
+declare -a info=()
 minute_scaled="$[ $time_scale / $slp ]"
 
 finish() {
@@ -19,7 +19,7 @@ finish() {
 	unset offset
 	unset slp
 	unset minute_scaled
-	unset information
+	unset info
 }
 trap finish EXIT
 
@@ -31,12 +31,6 @@ get_battery() {
 get_date() {
 	echo "$(date '+%a %b %d, %H:%M')"
 }
-
-#get_desktop() {
-#	desk="$(wmctrl -d)"
-#	desk="${desk%*  \* *}"
-#	desk="${desk: -1}"
-#}
 
 get_seconds_offset() {
 	offset="$(date '+%S')"
@@ -64,15 +58,15 @@ sync_time_update() {
 
 update_bar() {
 	if [ "$update_now" -eq "1" ]; then
-		echo "%{l} ${information[0]}%{c}${information[1]}%{r}${information[2]} "
+		echo "%{l} ${info[3]}${info[0]}%{c}${info[1]}%{r}${info[2]} "
 		update_now="0"
 	fi
 }
 
 # either just make coder remember the place in array that certain info
 # is, or do a nfancurve thing with two arrays that use each other
-update_information() {
-	information["$1"]="$2"
+update_info() {
+	info["$1"]="$2"
 }
 
 try_update() {
@@ -80,29 +74,57 @@ try_update() {
 	old_tmp="$2"
 	arr_pos="$3"
 	if [ "$tmp" != "$old_tmp" ]; then
-		update_information "$arr_pos" "$tmp"
+		update_info "$arr_pos" "$tmp"
 		update_now="1"
 	fi
 }
 
 init_values() {
+	IFS=$'\n'
+
 	# second offsets isn't to be displayed on panel
 	get_seconds_offset
+	update_information
 
-	# these are in display order (not needed, just easier to remember)
-	information+=( "$(./get_tasks.sh)" )
-	information+=( "$(get_date)" )
-	information+=( "$(get_battery)" )
+	info+=( "$(format_tasks_string)" )
+	info+=( "$(get_date)" )
+	info+=( "$(get_battery)" )
+	info+=( "$(update_desktop)" )
 }
 
 run_sec_cmds() {
 	# Checks if the information is stale before updating
-	try_update "$(./get_tasks.sh)" "{information[0]}" "0"
+#	try_update "$(./get_tasks.sh)" "{info[0]}" "0"
+	update_information
+	try_update "$(format_tasks_string)" "${info[0]}" "0"
+	try_update "$(update_desktop)" "${info[3]}" "3"
 	update_bar
 	sleep "$slp"
 }
 
+max_length="100"
+icon_empty="o"
+icon_used="x"
+format_len="0"
+fcol="ffffff"
+bcol="000000"
+desk_range="8"
+#task_len="100"
+update_tasks="0"
+old_active_win=""
+declare -a count=( "0" "0" "0" "0" "0" "0" "0" "0" "0" )
+active_win=""
+win_list=""
+declare -a task_hexs=()
+declare -a task_wins=()
+declare -a task_desk=()
+cdesk=""
+
 main() {
+	source get_info.sh
+	source get_tasks.sh
+	source get_desktop.sh
+
 	init_values
 
 	# Align when script checks for the current time to be accurate
@@ -118,14 +140,14 @@ main() {
 	while true; do
 		for i in `seq 0 4`; do
 			# Doesn't bother checking if the command gave a new value
-			update_information "1" "$(get_date)"
+			update_info "1" "$(get_date)"
 			# Essentially forces an update
 			update_now="1"
 			for j in `seq 0 $minute_scaled`; do
 				run_sec_cmds
 			done
 		done
-		update_information "2" "$(get_battery)"
+		update_info "2" "$(get_battery)"
 		sync_time_update
 	done
 }
