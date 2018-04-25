3734dbdf8d65eb97745c0342beb2b8e9bcf5baff
nan0s7 <scottwalters97@gmail.com>
2018-04-25 14:03:31 +1000

    Moved some things around and did a minor clean up

--- lb.sh
+++ lb.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# How long to sleep for
+# How long one unit of sleep should be
 slp="1"
+
+# Import all needed variables and functions from plugins
+source plugins/get_info.sh
+source plugins/get_time.sh
+source plugins/get_tasks.sh
+source plugins/get_desktop.sh
 
 # Variable initialisation
 update_now="1"
 declare -a info=()
-
-finish() {
-	unset per
-	unset tmp
-	unset time_scale
-	unset update_now
-	unset offset
-	unset slp
-	unset minute_scaled
-	unset info
-}
-trap finish EXIT
 
 get_battery() {
 	# This command will change in the future
@@ -31,24 +25,21 @@ update_bar() {
 	fi
 }
 
-# either just make coder remember the place in array that certain info
-# is, or do a nfancurve thing with two arrays that use each other
-update_info() {
+force_update() {
 	info["$1"]="$2"
 }
 
 try_update() {
 	tmp="$1"
-	old_tmp="$2"
-	arr_pos="$3"
+	arr_pos="$2"
+	old_tmp="$3"
 	if [ "$tmp" != "$old_tmp" ]; then
-		update_info "$arr_pos" "$tmp"
+		force_update "$arr_pos" "$tmp"
 		update_now="1"
 	fi
 }
 
 init_values() {
-	# second offsets isn't to be displayed on panel	get_seconds_offset
 	update_information
 
 	info+=( "$(format_tasks_string)" )
@@ -58,21 +49,15 @@ init_values() {
 }
 
 run_sec_cmds() {
-	# Checks if the information is stale before updating
 	update_information
-	try_update "$(format_tasks_string)" "${info[0]}" "0"
-	try_update "$(update_desktop)" "${info[3]}" "3"
+	# Checks if the information is stale before updating
+	try_update "$(format_tasks_string)" "0" "${info[0]}"
+	try_update "$(update_desktop)" "3" "${info[3]}"
 	update_bar
 	sleep "$slp"
 }
 
 main() {
-	# Import all needed variables and functions from plugins
-	source plugins/get_info.sh
-	source plugins/get_tasks.sh
-	source plugins/get_desktop.sh
-	source plugins/get_time.sh
-
 	init_values
 
 	# Align when script checks for the current time to be accurate
@@ -88,14 +73,14 @@ main() {
 	while true; do
 		for i in `seq 0 4`; do
 			# Doesn't bother checking if the command gave a new value
-			update_info "1" "$(get_date)"
+			force_update "1" "$(get_date)"
 			# Essentially forces an update
 			update_now="1"
 			for j in `seq 0 $minute_scaled`; do
 				run_sec_cmds
 			done
 		done
-		update_info "2" "$(get_battery)"
+		force_update "2" "$(get_battery)"
 		sync_time_update
 	done
 }
