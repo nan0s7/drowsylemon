be66ce731f1c5c150d7f924145b779623b25485c
nan0s7 <scottwalters97@gmail.com>
2018-04-26 16:46:22 +1000

    Minor tweaks

--- plugins/get_desktop.sh
+++ plugins/get_desktop.sh
@@ -8,20 +8,20 @@ desk_range="8"
 insert_icon() {
 	if [ "${count[$1]}" -gt "0" ]; then
 		# Icon used to indicate open windows
-		echo "x"
+		echo " x "
 	else
 		# Icon for an empty desktop
-		echo "o"
+		echo " o "
 	fi
 }
 
 update_desktop() {
 	for i in `seq 0 $[ $cdesk - 1 ]`; do
-		desktop_string+=" $(insert_icon $i) "
+		desktop_string+="$(insert_icon $i)"
 	done
-	desktop_string+="%{R} $(insert_icon $cdesk) %{R}"
+	desktop_string+="%{R}$(insert_icon $cdesk)%{R}"
 	for i in `seq $[ $cdesk + 1 ] $desk_range`; do
-		desktop_string+=" $(insert_icon $i) "
+		desktop_string+="$(insert_icon $i)"
 	done
-	echo -e "$desktop_string"
+	echo "$desktop_string"
 }
